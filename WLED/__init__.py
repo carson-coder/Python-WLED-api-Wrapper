ef7f0feecd8fd303bd3aa86cc90ab6f582648309
carson-coder <57198646+carson-coder@users.noreply.github.com>
2022-07-25 14:05:21 -0400

    Bug Fixes And run.py prints info of wled strip

--- WLED/__init__.py
+++ WLED/__init__.py
@@ -1,5 +1,5 @@
 import requests
-import Exceptions
+from . import Exceptions
 
 pallets = "pal"
 effects = "fx"
@@ -71,14 +71,13 @@ class Wled():
     def get_effects(self):
         return(self.raw_raw_data["effects"])
     def __str__(self):
-        return(f"""
-                WLED Strip:
-                    IP: {self.hostname}
-                    Endpoint: {self.endpoint}
-                    Current Segment: {self.seg}:
-                        On: {self.data["on"]}
-                        Brightness: {self.data["bri"]}
-                        Color1: {self.data["col"][0]}
-                        Color2: {self.data["col"][1]}
-                        Color3: {self.data["col"][2]}
+        return(f"""WLED Strip:
+    IP: {self.hostname}
+    Endpoint: {self.endpoint}
+    Current Segment: {self.seg}:
+        On: {self.data["on"]}
+        Brightness: {self.data["bri"]}
+        Color1: {self.data["col"][0]}
+        Color2: {self.data["col"][1]}
+        Color3: {self.data["col"][2]}
                """)
